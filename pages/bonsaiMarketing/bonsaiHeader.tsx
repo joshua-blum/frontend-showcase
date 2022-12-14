dc86808b77b857f9a699461a7fee0823191855b2
Joshua Blum <blum.r.joshua@gmail.com>
2022-12-14 11:05:51 -0800

    Create initial header layout

--- pages/bonsaiMarketing/bonsaiHeader.tsx
+++ pages/bonsaiMarketing/bonsaiHeader.tsx
@@ -1,5 +1,46 @@
+import styles from '../../styles/bonsaiMarketing/Header.module.css';
+import Image from 'next/image'
+import { setSyntheticLeadingComments } from 'typescript';
+
 export default function BonsaiHeader(){
     return (
-        <div>Here is where the header will go</div>
+        <div className={styles.header}>
+            <div className={styles.headerElement}>
+                <Image
+                    className={styles.logo}
+                    width={150}
+                    height={40}
+                    src="/images/bonsaiMarketing/bonsai_logo.svg"
+                    alt="Bonsai logo" />
+            </div>
+            <div className={styles.headerElement}>
+                <div className="dropdown">
+                    <button className={`btn btn-secondary dropdown-toggle ${styles.centerElement}`} type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Product
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a className="dropdown-item" href="#">Action</a>
+                        <a className="dropdown-item" href="#">Another action</a>
+                        <a className="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </div>
+                <div className="dropdown">
+                    <button className={`btn btn-secondary dropdown-toggle ${styles.centerElement}`} type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Templates
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a className="dropdown-item" href="#">Action</a>
+                        <a className="dropdown-item" href="#">Another action</a>
+                        <a className="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </div>
+                <button className={styles.centerElement}>Pricing</button>
+                <button className={styles.centerElement}>Reviews</button>
+            </div>
+            <div className={styles.headerElement}>
+                <button className={`btn ${styles.button}`}>LOG IN</button>
+                <button className={`btn ${styles.button}`}>START FREE</button>
+            </div>
+        </div>
     )
 }
