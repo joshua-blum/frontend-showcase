2690853b61147b4bbe679cd6ac3738621cbc763b
Joshua Blum <blum.r.joshua@gmail.com>
2022-12-28 11:24:16 -0800

    Create mock data pull for plan cards

--- pages/bonsaiMarketing/bonsaiPlansAndPricing.tsx
+++ pages/bonsaiMarketing/bonsaiPlansAndPricing.tsx
@@ -1,45 +1,61 @@
 import styles from '../../styles/bonsaiMarketing/PlansAndPricing.module.css';
 import React, {useState, useEffect} from 'react';
-import { setDefaultResultOrder } from 'dns';
+
+function formatPlanCards(planCardList: any){
+    return planCardList.map(
+        (planCard: 
+            {
+                id: string, 
+                title: string, 
+                description: string,
+                priceMonthly: number,
+                priceYearly: number,
+                details: [],
+                isPopular: boolean
+            }
+        ) => {   
+        return (
+            <div className={styles.planCard}>
+                <h1 className={styles.planTitle}>{planCard.title}</h1>
+                <p className={styles.planDescription}>{planCard.description}</p>
+                <div className={`${styles.planPrice} ${styles.monthlyPlan}`}>${planCard.priceMonthly} <span className={styles.planSubtitle}>/MONTH</span></div>
+                <div className={`${styles.planPrice} ${styles.yearlyPlan}`}>${planCard.priceYearly}  <span className={styles.planSubtitle}>/MONTH</span></div>
+                <div className={`${styles.planSubtitle} ${styles.yearlyPlan}`}>BILLED MONTHLY</div>
+                <br className={styles.planBreak}/>
+                <ul className={styles.planDetails}>
+                    {planCard.details ? 
+                        planCard.details.map((detail: string) => {return <li key={detail} className={styles.listElement}>{detail}</li>}) 
+                        : <h1>Loading...</h1>}
+                </ul>
+            </div>
+            )
+        })
+}
 
 
 export default function BonsaiPlansAndPricing(){
-    const [planCardList, setPlanCardList] = useState([<h1>Loading</h1>]);
+    const [planCardList, setPlanCardList] = useState([<h1>Loading...</h1>]);
     const [isYearly, setIsYearly] = useState(false);
 
     useEffect(() => {
         fetch('http://localhost:3000/data/bonsaiMarketing/plansAndPricing.json')
         .then((res) => res.json())
         .then((json) => [...json.attributes])
-        .then(setPlanCardList)
-    }, [planCardList])
+        .then((res) => setPlanCardList(res))
+    }, [])
+
+    const innerHTML = formatPlanCards(planCardList);
+
 
     return (
+        <>
         <div className={styles.plansAndPricing}>
             <div className={styles.header}>
                 <div className={styles.title}>Plans &#38; Pricing</div>
                 <div className={styles.toggleButton}>Monthly button Yearly</div>
             </div>
             <div className={styles.planCardsList}>
-                <div className={styles.planCard}>
-                    <h1 className={styles.planTitle}>Card 1</h1>
-                    <p className={styles.planDescription}>Here is a stock description</p>
-                    <div className={`${styles.planPrice} ${styles.monthlyPlan}`}>$24 <span className={styles.planSubtitle}>/MONTH</span></div>
-                    <div className={`${styles.planPrice} ${styles.yearlyPlan}`}>$17  <span className={styles.planSubtitle}>/MONTH</span></div>
-                    <div className={`${styles.planSubtitle} ${styles.yearlyPlan}`}>BILLED MONTHLY</div>
-                    <br className={styles.planBreak}/>
-                    <ul className={styles.planDetails}>
-                        <li key={1} className={styles.listElement}>info deet 1</li>
-                        <li key={2} className={styles.listElement}>info deet 2</li>
-                        <li key={3} className={styles.listElement}>info deet 3</li>
-                    </ul>
-                </div>
-                <div className={styles.planCard}>
-                    Card 2
-                </div>
-                <div className={styles.planCard}>
-                    Card 3
-                </div>
+                {innerHTML}
             </div>
             <div className={styles.header}>Super charge your work with add-ons</div>
             <div className={styles.addOnsList}>
@@ -54,5 +70,6 @@ export default function BonsaiPlansAndPricing(){
                 </div>
             </div>
         </div>
+        </>
     )
 }
