864d2fa24c16d171a921be33c759212825785cc8
Joshua Blum <blum.r.joshua@gmail.com>
2023-01-05 10:33:17 -0800

    Implement FAQ with React-Bootstrap accordion

--- pages/bonsaiMarketing/bonsaiFAQ.tsx
+++ pages/bonsaiMarketing/bonsaiFAQ.tsx
@@ -1,22 +1,61 @@
 import styles from '/styles/bonsaiMarketing/FAQ.module.css';
-import React, {useState, useEffect} from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 
 export default function BonsaiFAQ(){
-    // const[visibleStatus, setVisibleStatus] = useState({ {1: false}; {2: false}; {3: false}; {4: false} } )
-
     return (
         <div className={styles.faq}>
             <h3 className={styles.title}>Frequently Asked Questions</h3>
-            <div className={styles.questionGroup}>
-                <div className={styles.question}>Here is a question? &nbsp;
-                    <button className={styles.dropDownButton} 
-                    // onClick={() => {useState(previousState => 
-                    //     return [!previousState[0], previousState[1], previousState[2], previousState[3]]
-                    // )}}
-                    >{`>`}</button>
-                </div>
-                <div className={styles.answer} id="demo" >Here is an answer</div>
-            </div>
+                <Accordion className={styles.questionGroup} flush alwaysOpen>
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>How does the 14 day free trial work? &nbsp;</Accordion.Header>
+                        <Accordion.Body className={styles.answer}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>Can I change plans anytime? &nbsp;</Accordion.Header>
+                        <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="2">
+                        <Accordion.Header>How do I pause my Bonsai subscription? &nbsp;</Accordion.Header>
+                        <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="3">
+                        <Accordion.Header>What is your refund policy? &nbsp;</Accordion.Header>
+                        <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
         </div>
     )
 }
+
