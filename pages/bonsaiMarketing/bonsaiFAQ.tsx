772a58964e1cd28f2d4c9cb4e15da4cee1e3edf9
Joshua Blum <blum.r.joshua@gmail.com>
2023-01-04 17:52:15 -0800

    Begin to implement toggle FAQ

--- pages/bonsaiMarketing/bonsaiFAQ.tsx
+++ pages/bonsaiMarketing/bonsaiFAQ.tsx
@@ -1,5 +1,22 @@
+import styles from '/styles/bonsaiMarketing/FAQ.module.css';
+import React, {useState, useEffect} from 'react';
+
 export default function BonsaiFAQ(){
+    // const[visibleStatus, setVisibleStatus] = useState({ {1: false}; {2: false}; {3: false}; {4: false} } )
+
     return (
-        <div>Here is where the FAQ will go</div>
+        <div className={styles.faq}>
+            <h3 className={styles.title}>Frequently Asked Questions</h3>
+            <div className={styles.questionGroup}>
+                <div className={styles.question}>Here is a question? &nbsp;
+                    <button className={styles.dropDownButton} 
+                    // onClick={() => {useState(previousState => 
+                    //     return [!previousState[0], previousState[1], previousState[2], previousState[3]]
+                    // )}}
+                    >{`>`}</button>
+                </div>
+                <div className={styles.answer} id="demo" >Here is an answer</div>
+            </div>
+        </div>
     )
 }
