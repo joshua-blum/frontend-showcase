ac4df6669ebe1f3d4437e6098c9a07eb3690d015
Joshua Blum <blum.r.joshua@gmail.com>
2023-01-04 17:04:54 -0800

    Implement promotional jumbotron

--- pages/bonsaiMarketing/bonsaiPromo.tsx
+++ pages/bonsaiMarketing/bonsaiPromo.tsx
@@ -1,5 +1,17 @@
+import styles from '/styles/bonsaiMarketing/Promo.module.css';
+
 export default function BonsaiPromo(){
     return (
-        <div>Here is where the promo jumbotron will go</div>
+        <div className={styles.promo}>
+            <div className={`jumbotron ${styles.jumbotron}`}>
+                <div className={styles.message}>
+                    <h4 className="display-4">It's <span className={styles.green}>your</span> business.</h4>
+                    <h4 className="display-4">We're here to help it grow.</h4>
+                </div>
+                <p className={`lead ${styles.buttonGroup}`}>
+                    <a className={`btn btn-primary ${styles.button}`} href="#" role="button">START FREE</a>
+                </p>
+            </div>
+        </div>
     )
 }
