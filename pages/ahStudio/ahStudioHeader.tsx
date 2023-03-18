b082715a5e269062f01604f02c929ec6543cd94b
Joshua Blum <blum.r.joshua@gmail.com>
2023-03-17 21:13:34 -0700

    Add formatted header

--- pages/ahStudio/ahStudioHeader.tsx
+++ pages/ahStudio/ahStudioHeader.tsx
@@ -1,3 +1,13 @@
+import styles from '/styles/ahStudio/Header.module.css';
+
 export default function Header(){
-    return <h1>This is where the floating header will go</h1>
+    return (
+        <div className={styles.header}>
+            <div className={styles.sliderGroup}>
+                <p className={`${styles.sliderHeading} ${styles.currentPage}`}>Home</p>
+                <p className={styles.sliderHeading}>Profile</p>
+                <p className={styles.sliderHeading}>Contact</p>
+            </div>
+        </div>
+    )
 }
