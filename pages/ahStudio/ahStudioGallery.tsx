fdc64438643f542a4ebc4e7b904e188bf6d6f520
Joshua Blum <blum.r.joshua@gmail.com>
2023-03-17 21:35:21 -0700

    Add gallery

--- pages/ahStudio/ahStudioGallery.tsx
+++ pages/ahStudio/ahStudioGallery.tsx
@@ -1,3 +1,15 @@
+import styles from '/styles/ahStudio/Gallery.module.css';
+import Image from 'react-bootstrap/Image';
+
 export default function Gallery(){
-    return <h3>== various images will go here ==</h3>
+    return (
+        <div className={styles.galleryContainer}>
+            <Image className={styles.image} src='/images/ahStudio/product_1.jpeg' />         
+            <Image className={styles.image} src='/images/ahStudio/product_2.jpeg' /> 
+            <Image className={styles.image} src='/images/ahStudio/product_3.jpeg' /> 
+            <Image className={styles.image} src='/images/ahStudio/product_4.jpeg' /> 
+            <Image className={styles.image} src='/images/ahStudio/product_5.jpeg' /> 
+            <Image className={styles.image} src='/images/ahStudio/product_6.jpeg' />         
+        </div>
+    ) 
 }
