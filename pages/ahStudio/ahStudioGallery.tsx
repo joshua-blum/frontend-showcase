5900fc78411cf5d3efc7ccaf1a66d0db5e80ee6b
Joshua Blum <blum.r.joshua@gmail.com>
2023-03-21 17:18:33 -0700

    Add hover effect for gallery

--- pages/ahStudio/ahStudioGallery.tsx
+++ pages/ahStudio/ahStudioGallery.tsx
@@ -4,12 +4,48 @@ import Image from 'react-bootstrap/Image';
 export default function Gallery(){
     return (
         <div className={styles.galleryContainer}>
-            <Image className={styles.image} src='/images/ahStudio/product_1.jpeg' />         
-            <Image className={styles.image} src='/images/ahStudio/product_2.jpeg' /> 
-            <Image className={styles.image} src='/images/ahStudio/product_3.jpeg' /> 
-            <Image className={styles.image} src='/images/ahStudio/product_4.jpeg' /> 
-            <Image className={styles.image} src='/images/ahStudio/product_5.jpeg' /> 
-            <Image className={styles.image} src='/images/ahStudio/product_6.jpeg' />         
+            <div className={styles.imageBox}>
+                <Image className={styles.image} src='/images/ahStudio/product_1.jpeg' />
+                <div className={styles.overlay}>
+                    <p className={styles.subtitle}>Subtitle</p>
+                    <p className={styles.icon}>{`->`}</p>    
+                </div>  
+            </div>
+            <div className={styles.imageBox}>
+                <Image className={styles.image} src='/images/ahStudio/product_2.jpeg' />  
+                <div className={styles.overlay}>
+                    <p className={styles.subtitle}>Subtitle</p>
+                    <p className={styles.icon}>{`->`}</p>    
+                </div>  
+            </div>    
+            <div className={styles.imageBox}>
+                <Image className={styles.image} src='/images/ahStudio/product_3.jpeg' />  
+                <div className={styles.overlay}>
+                    <p className={styles.subtitle}>Subtitle</p>
+                    <p className={styles.icon}>{`->`}</p>    
+                </div>  
+            </div>    
+            <div className={styles.imageBox}>
+                <Image className={styles.image} src='/images/ahStudio/product_4.jpeg' />  
+                <div className={styles.overlay}>
+                    <p className={styles.subtitle}>Subtitle</p>
+                    <p className={styles.icon}>{`->`}</p>    
+                </div>  
+            </div>    
+            <div className={styles.imageBox}>
+                <Image className={styles.image} src='/images/ahStudio/product_5.jpeg' />  
+                <div className={styles.overlay}>
+                    <p className={styles.subtitle}>Subtitle</p>
+                    <p className={styles.icon}>{`->`}</p>    
+                </div>  
+            </div>    
+            <div className={styles.imageBox}>
+                <Image className={styles.image} src='/images/ahStudio/product_6.jpeg' />  
+                <div className={styles.overlay}>
+                    <p className={styles.subtitle}>Subtitle</p>
+                    <p className={styles.icon}>{`->`}</p>    
+                </div>  
+            </div>           
         </div>
     ) 
 }
