92a512e044630b0899f80d249fbe40b09c2e05d0
Joshua Blum <blum.r.joshua@gmail.com>
2023-03-22 16:18:14 -0700

    Implement sticky header

--- pages/ahStudio/ahStudioHeader.tsx
+++ pages/ahStudio/ahStudioHeader.tsx
@@ -1,8 +1,16 @@
 import styles from '/styles/ahStudio/Header.module.css';
+import {useState} from 'react';
 
 export default function Header(){
+    const [isTransparent, setIsTransparent] = useState(false);
+    if(typeof window !== 'undefined'){
+    window.addEventListener("scroll", () => {
+        if(window.scrollY >= 100) setIsTransparent(true);
+        else setIsTransparent(false);
+    })}
+    
     return (
-        <div className={styles.header}>
+        <div className={`${styles.header} ${isTransparent ? styles.transparent : null}`}>
             <div className={styles.sliderGroup}>
                 <p className={`${styles.sliderHeading} ${styles.currentPage}`}>Home</p>
                 <p className={styles.sliderHeading}>Profile</p>
