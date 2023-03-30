e759da3a39d4b6c423447bd9098920e16ac0b2b8
Joshua Blum <blum.r.joshua@gmail.com>
2023-03-30 11:20:26 -0700

    Initial header layout

--- pages/monsterMouse/monsterMouseNav.tsx
+++ pages/monsterMouse/monsterMouseNav.tsx
@@ -1,9 +1,52 @@
 import styles from '/styles/monsterMouse/Nav.module.css';
+import Image from 'next/image';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faInstagram, faTiktok, faTwitter, faTwitch, faFacebook, faDiscord  } from '@fortawesome/free-brands-svg-icons'
+
+
 
 export default function Nav(){
     return (
         <>
-            <h2>This is where the sticky navbar will be!!</h2>
+            <header className={styles.header}>
+                <div className={styles.logo}>
+                    <Image src='/images/monsterMouse/monsterMouse_logo_2.png'
+                        className={styles.logoImage}
+                        width={100} 
+                        height={100}
+                        alt="Logo" />
+                </div>
+                <div className={styles.sidebar}>
+                    <div className={styles.menu}>
+                        Menu
+                    </div>
+                    <br />
+                    <FontAwesomeIcon 
+                        className={styles.image}
+                        icon={faInstagram}
+                         />
+                    <FontAwesomeIcon 
+                        className={styles.image}
+                        icon={faTiktok}
+                         />
+                    <FontAwesomeIcon 
+                        className={styles.image}
+                        icon={faTwitter}
+                         />
+                    <FontAwesomeIcon 
+                        className={styles.image}
+                        icon={faTwitch}
+                         />
+                    <FontAwesomeIcon 
+                        className={styles.image}
+                        icon={faFacebook}
+                         />
+                    <FontAwesomeIcon 
+                        className={styles.image}
+                        icon={faDiscord}
+                         />
+                 </div>
+            </header>
         </>
     )
 }
