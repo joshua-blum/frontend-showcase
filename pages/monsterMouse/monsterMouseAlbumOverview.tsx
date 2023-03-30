5e24cb826e2666aeade6f161c11a186ab1f347df
Joshua Blum <blum.r.joshua@gmail.com>
2023-03-30 14:35:50 -0700

    Implement basic album overview

--- pages/monsterMouse/monsterMouseAlbumOverview.tsx
+++ pages/monsterMouse/monsterMouseAlbumOverview.tsx
@@ -4,23 +4,23 @@ import Image from 'next/image';
 export default function AlbumOverview(){
     return (
         <section className={styles.overview}>
+            <div className={styles.namesAndActions}>
+                <h2 className={styles.albumName}>Vikings Memories</h2>
+                <h3 className={styles.artistName}>SKÁLD</h3>
+                <div className={styles.buttonOptions}>
+                    <button className={`${styles.button} ${styles.listenNowButton}`}>Listen Now</button>
+                    <button className={`${styles.button} ${styles.shareButton}`}>Share</button>
+                </div>
+            </div>
             <div className={styles.album}>
-                <h4 className={styles.albumSubtitle}>Tribal - Released Month DD, YYYY</h4>
                 <Image
                     className={styles.albumArt}
                     src="/images/monsterMouse/skald_album_cover.jpg"
-                    width={100}
-                    height={100}
+                    width={500}
+                    height={500}
                     alt="Album art"                    
                     />
-            </div>
-            <div className={styles.namesAndActions}>
-                <h2 className={styles.albumName}>Vikings Memories</h2>
-                <h3 className={styles.artistName}>Skald</h3>
-                <div className={styles.buttonOptions}>
-                    <button className={styles.listenNowButton}>Listen Now</button>
-                    <button className={styles.shareButton}>Share</button>
-                </div>
+                <h4 className={styles.albumSubtitle}><span className={styles.category}>Tribal</span> - Released October 09, 2020</h4>
             </div>
         </section>
     )
