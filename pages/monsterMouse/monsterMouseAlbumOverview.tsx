d7b83080b0f8d595d1381ca1373b0776efdf463b
Joshua Blum <blum.r.joshua@gmail.com>
2023-03-30 11:52:32 -0700

    Create background for album overview

--- pages/monsterMouse/monsterMouseAlbumOverview.tsx
+++ pages/monsterMouse/monsterMouseAlbumOverview.tsx
@@ -1,9 +1,27 @@
 import styles from '/styles/monsterMouse/AlbumOverview.module.css';
+import Image from 'next/image';
 
 export default function AlbumOverview(){
     return (
-        <>
-            <h2>This is where the album art, title, and larger buttons will be!!</h2>
-        </>
+        <section className={styles.overview}>
+            <div className={styles.album}>
+                <h4 className={styles.albumSubtitle}>Tribal - Released Month DD, YYYY</h4>
+                <Image
+                    className={styles.albumArt}
+                    src="/images/monsterMouse/skald_album_cover.jpg"
+                    width={100}
+                    height={100}
+                    alt="Album art"                    
+                    />
+            </div>
+            <div className={styles.namesAndActions}>
+                <h2 className={styles.albumName}>Vikings Memories</h2>
+                <h3 className={styles.artistName}>Skald</h3>
+                <div className={styles.buttonOptions}>
+                    <button className={styles.listenNowButton}>Listen Now</button>
+                    <button className={styles.shareButton}>Share</button>
+                </div>
+            </div>
+        </section>
     )
 }
